c85e8a0c25275c30bf5eee7d27269bf0dd6e72a2
CesarXiu <131189969+CesarXiu@users.noreply.github.com>
2024-10-27 19:28:18 -0500

    dado

--- src/setget.kt
+++ src/setget.kt
@@ -1,6 +1,16 @@
 
-class dado constructor(valor:int) {
-
+class dado {
+        var valor:Int = 0
+        set(value){
+            if(value>6 || value<1)
+                field = 1
+            else
+                field = value
+        }
+    fun aleatorio() = ((Math.random()* (6 + 1 - 1)) + 1).toInt()
+    fun print(){
+        println(this.valor)
+    }
 }
 fun main(args: Array<String>) {
     /*
@@ -8,5 +18,11 @@ fun main(args: Array<String>) {
         Definir dos métodos, uno que genere un número aleatorio entre 1 y 6 y otro que lo imprima.
         Al constructor llega el valor inicial que debe tener el dado (tratar de enviarle el número 7)
     */
+    val dado1:dado = dado()
+    dado1.valor = 7
+    dado1.print()
+    println(dado1.aleatorio())
+    dado1.valor = dado1.aleatorio()
+    dado1.print()
 
 }
